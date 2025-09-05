9438c91907fe7edccd1170ecc63bd2f097d71cdf
Anjali ojha <oanjali724@gmail.com>
2025-09-05 14:37:13 +0530

    Delete projects/dict.js

--- projects/dict.js
+++
@@ -1,6 +0,0 @@
-const form = document .querySelector('form');
-const result = document .querySelector('.result');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // don't reload the page
-});
